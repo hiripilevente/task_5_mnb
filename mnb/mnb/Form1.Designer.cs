2466baff1e44b44a0648f780e0df423500963940
HiripiLevi@gmail.com <HiripiLevi@gmail.com>
2022-12-14 13:32:44 +0100

    5_ xml process

--- mnb/mnb/Form1.Designer.cs
+++ mnb/mnb/Form1.Designer.cs
@@ -31,14 +31,16 @@
             this.cbx1 = new System.Windows.Forms.ComboBox();
             this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
             this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // cbx1
             // 
             this.cbx1.FormattingEnabled = true;
-            this.cbx1.Location = new System.Drawing.Point(498, 39);
+            this.cbx1.Location = new System.Drawing.Point(476, 37);
             this.cbx1.Name = "cbx1";
-            this.cbx1.Size = new System.Drawing.Size(121, 24);
+            this.cbx1.Size = new System.Drawing.Size(143, 24);
             this.cbx1.TabIndex = 0;
             // 
             // dateTimePicker1
@@ -50,22 +52,33 @@
             // 
             // dateTimePicker2
             // 
-            this.dateTimePicker2.Location = new System.Drawing.Point(260, 39);
+            this.dateTimePicker2.Location = new System.Drawing.Point(245, 39);
             this.dateTimePicker2.Name = "dateTimePicker2";
             this.dateTimePicker2.Size = new System.Drawing.Size(200, 22);
             this.dateTimePicker2.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 67);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(607, 523);
+            this.dataGridView1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1216, 602);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.dateTimePicker1);
             this.Controls.Add(this.cbx1);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -75,6 +88,7 @@
         private System.Windows.Forms.ComboBox cbx1;
         private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- mnb/mnb/Form1.cs
+++ mnb/mnb/Form1.cs
@@ -1,4 +1,5 @@
-﻿using mnb.MnbServiceReference;
+﻿using mnb.Entities;
+using mnb.MnbServiceReference;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -10,39 +11,66 @@ using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace mnb
 {
 
     public partial class Form1 : Form
     {
+        BindingList<RateData> Rates = new BindingList<RateData>();
+
 
         public Form1()
         {
-            GetRates();
+            dataGridView1.DataSource= Rates;
+
             InitializeComponent();
         }
 
-        private string GetRates()
+        private void ReadXml()
         {
-            MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
-            GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody();
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(GetRates());
+            foreach (XmlElement item in xml.DocumentElement)
             {
-                string currencyNames = cbx1.SelectedItem.ToString(),
-                startDate = dateTimePicker1.Value.ToString(),
-                endDate = dateTimePicker2.Value.ToString()
-            };
-            GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
-            string result = response.GetExchangeRatesResult;
-            //MessageBox.Show(result);
-
-            mnbService.Close();
-            return result;
+                if (item.ChildNodes[0] != null)
+                {
+                    RateData rd = new RateData();
+                    Rates.Add(rd);
+                    rd.Currency = item.ChildNodes[0].Attributes["curr"].Value;
+                    rd.Date = Convert.ToDateTime(item.Attributes["date"].Value);
+                    decimal unit = Convert.ToDecimal(item.ChildNodes[0].Attributes["unit"].Value);
+                    decimal value = Convert.ToDecimal(item.ChildNodes[0].InnerText);
+                    if (unit != 0)
+                    {
+                        rd.Value = value / unit;
+                    }
+                    else
+                    {
+                        rd.Value = value;
+                    }
+                }
+            }
         }
 
-        private void Form1_Load(object sender, EventArgs e)
-        {
+            private string GetRates()
+            {
+                MNBArfolyamServiceSoapClient mnbService = new MNBArfolyamServiceSoapClient();
+                GetExchangeRatesRequestBody request = new GetExchangeRatesRequestBody()
+                {
+                    currencyNames = cbx1.SelectedItem.ToString(),
+                    startDate = dateTimePicker1.Value.ToString(),
+                    endDate = dateTimePicker2.Value.ToString()
+                };
+                GetExchangeRatesResponseBody response = mnbService.GetExchangeRates(request);
+                string result = response.GetExchangeRatesResult;
+                //MessageBox.Show(result);
 
-        }
+                mnbService.Close();
+                return result;
+            }
+        
+
     }
 }
